41a3f671c4a006f921671b0d9f490c741f9b7083
Yurav <yuravbeo@gmail.com>
2023-09-21 09:48:52 +0200

    Read from API correctly

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import requests
 
 app = Flask(__name__)
 
+def fetch_data_from_api(endpoint):
+    api_key = "e6506999-8738-4866-a13f-2a2cfb14ba99"
+    headers = {"x-api-key": api_key}
+    url = f"https://hackathon.syftanalytics.com/api/{endpoint}"
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data', [])
+    else:
+        return []
+
 @app.route('/')
 def home():
-    return render_template('index.html', title='Bakery Home')
+    contacts = fetch_data_from_api('contacts')
+
+    num_suppliers = sum([1 for contact in contacts if contact.get('is_supplier')])
+    num_customers = sum([1 for contact in contacts if contact.get('is_customer')])
+
+    return render_template('index.html', title='Business Dashboard', num_suppliers=num_suppliers, num_customers=num_customers)
+
+@app.route('/data_analytics')
+def data_analytics():
+    return render_template('data_analytics.html', title='Data Analytics')
 
 @app.route('/about')
 def about():
