c23eb5b9614d31435ffb84b5c0b1bf5ff8c93e53
Yurav <yuravbeo@gmail.com>
2023-09-22 15:53:01 +0200

    Edit for two graphs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, jsonify
 import requests
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -80,16 +81,29 @@ def data_analytics():
                 
     # Calculate money coming in (revenue)
     revenue_invoices = [invoice for invoice in invoices if invoice['is_sale'] and invoice['paid']]
+    
+    revenue_values = [invoice['total'] for invoice in revenue_invoices]
     revenue_dates = [invoice['issue_date'] for invoice in revenue_invoices]
     revenue_dates = [date[:10] for date in revenue_dates]
-    revenue_values = [invoice['total'] for invoice in revenue_invoices]
+
+    expense_invoices = [invoice for invoice in invoices if not invoice['is_sale'] and invoice['paid']]
+    
+    expense_values = [invoice['total'] for invoice in expense_invoices]
+    expense_dates = [invoice['issue_date'] for invoice in expense_invoices]
+    expense_dates = [date[:10] for date in expense_dates]
+
+    # Filter the data to remove values above 10000
+    filtered_revenue_values = [value for value in revenue_values if value <= 10000]
+    filtered_expense_values = [value for value in expense_values if value <= 10000]
 
     # Render the template with the calculated values
     return render_template('data_analytics.html', title='Data Analytics', contacts=contacts, 
                             missing_supplier_emails=missing_supplier_emails, missing_supplier_phones=missing_supplier_phones, 
                             missing_customer_emails=missing_customer_emails, missing_customer_phones=missing_customer_phones,
                             items=items, low_inventory_items=low_inventory_items, total_value_of_inventory=total_value_of_inventory,
-                            total_items_in_inventory=total_items_in_inventory, revenue_dates=revenue_dates, revenue_values=revenue_values)
+                            total_items_in_inventory=total_items_in_inventory, revenue_dates=revenue_dates, revenue_values=revenue_values,
+                            expense_values=expense_values, expense_dates=expense_dates, filtered_revenue_values=filtered_revenue_values,
+                            filtered_expense_values=filtered_expense_values)
 
 
 @app.route('/about')
