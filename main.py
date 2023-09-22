abbd7a2799e2d69c05a9c8ce12c68d2751f89392
Yurav <yuravbeo@gmail.com>
2023-09-22 21:01:18 +0200

    Final edits without full comments

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def fetch_data_from_api(endpoint):
     api_key = "e6506999-8738-4866-a13f-2a2cfb14ba99"
     headers = {"x-api-key": api_key}
     url = f"https://hackathon.syftanalytics.com/api/{endpoint}"
-
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json().get('data', [])
@@ -32,11 +31,11 @@ def home():
     total_items_in_inventory = sum(item['quantity_on_hand'] for item in items)
 
     # Considering paid status, exchange rate, and currency for the total amount to be received and paid
-    total_amount_to_be_received = sum(invoice['amount_due'] * invoice['exchange_rate'] if invoice['is_sale'] and not invoice['paid'] and invoice['currency'] != 'ZAR' else invoice['amount_due'] for invoice in invoices if invoice['is_sale'] and not invoice['paid'])
+    total_amount_to_be_received = sum(invoice['amount_due'] if invoice['is_sale'] and not invoice['paid'] and invoice['currency'] != 'ZAR' else invoice['amount_due'] for invoice in invoices if invoice['is_sale'] and not invoice['paid'])
     total_amount_to_be_paid = sum(invoice['amount_due'] * invoice['exchange_rate'] if not invoice['is_sale'] and not invoice['paid'] and invoice['currency'] != 'ZAR' else invoice['amount_due'] for invoice in invoices if not invoice['is_sale'] and not invoice['paid'])
 
     # Considering exchange rate and currency for income and expense from invoices
-    income_invoices = sum(invoice['total'] * invoice['exchange_rate'] if invoice['is_sale'] and invoice['currency'] != 'ZAR' else invoice['total'] for invoice in invoices if invoice['is_sale'])
+    income_invoices = sum(invoice['total']if invoice['is_sale'] and invoice['currency'] != 'ZAR' and not invoice['is_sale'] else invoice['total'] for invoice in invoices if invoice['is_sale'])
     expense_invoices = sum(invoice['total'] * invoice['exchange_rate'] if not invoice['is_sale'] and invoice['currency'] != 'ZAR' else invoice['total'] for invoice in invoices if not invoice['is_sale'])
 
     # Considering exchange rate and currency for income and expense from payments
@@ -54,6 +53,10 @@ def home():
                             expense_invoices=expense_invoices,
                             income_payments=income_payments,
                             expense_payments=expense_payments)
+
+# Find contact by ID in a list of contacts
+def find_contact_by_id(contact_id, contacts):
+    return next((contact for contact in contacts if contact['id'] == contact_id), None)
 
 @app.route('/data_analytics')
 def data_analytics():
@@ -82,7 +85,7 @@ def data_analytics():
     # Calculate money coming in (revenue)
     revenue_invoices = [invoice for invoice in invoices if invoice['is_sale'] and invoice['paid']]
     
-    revenue_values = [invoice['total'] for invoice in revenue_invoices]
+    revenue_values = [invoice['total'] * invoice['exchange_rate'] if invoice['currency'] != 'ZAR' and not invoice['is_sale'] else invoice['total'] for invoice in revenue_invoices]
     revenue_dates = [invoice['issue_date'] for invoice in revenue_invoices]
     revenue_dates = [date[:10] for date in revenue_dates]
 
@@ -96,6 +99,46 @@ def data_analytics():
     filtered_revenue_values = [value for value in revenue_values if value <= 10000]
     filtered_expense_values = [value for value in expense_values if value <= 10000]
 
+    # Calculate outstanding income and expense invoices
+    outstanding_income_invoices = [
+        {
+            'invoice_id': invoice['id'],
+            'contact_id': find_contact_by_id(invoice['contact_id'], contacts),
+            'amount_due': invoice['total'] - invoice['amount_due'],
+            'due_date': invoice['due_date'][:10]
+        }
+        for invoice in invoices if invoice['is_sale'] and not invoice['paid'] and (invoice['total'] - invoice['amount_due']) != 0
+    ]
+
+    outstanding_expense_invoices = [
+        {
+            'invoice_id': invoice['id'],
+            'contact_id': find_contact_by_id(invoice['contact_id'], contacts),
+            'amount_due': (invoice['total'] - invoice['amount_due']) * invoice['exchange_rate'] if invoice['currency'] != 'ZAR' else (invoice['total'] - invoice['amount_due']),
+            'due_date': invoice['due_date'][:10]
+        }
+        for invoice in invoices if not invoice['is_sale'] and not invoice['paid'] and (invoice['total'] - invoice['amount_due']) != 0
+    ]
+
+   # Initialize totals to 0 to ensure they have a value regardless of whether invoices exist
+    outstandingIncomeTotal = 0.00
+    outstandingExpenseTotal = 0.00
+
+    # Calculate outstandingIncomeTotal only if there are outstanding income invoices
+    if outstanding_income_invoices:
+        outstandingIncomeTotal = sum(invoice['amount_due'] for invoice in outstanding_income_invoices)
+
+    # Calculate outstandingExpenseTotal only if there are outstanding expense invoices
+    if outstanding_expense_invoices:
+        outstandingExpenseTotal = sum(invoice['amount_due'] for invoice in outstanding_expense_invoices)
+
+        # Format the totals to two decimal places
+    outstandingIncomeTotal = "{:.2f}".format(outstandingIncomeTotal)
+    outstandingExpenseTotal = "{:.2f}".format(outstandingExpenseTotal)
+
+    print(outstandingIncomeTotal)
+    print(outstandingExpenseTotal)
+
     # Render the template with the calculated values
     return render_template('data_analytics.html', title='Data Analytics', contacts=contacts, 
                             missing_supplier_emails=missing_supplier_emails, missing_supplier_phones=missing_supplier_phones, 
@@ -103,7 +146,9 @@ def data_analytics():
                             items=items, low_inventory_items=low_inventory_items, total_value_of_inventory=total_value_of_inventory,
                             total_items_in_inventory=total_items_in_inventory, revenue_dates=revenue_dates, revenue_values=revenue_values,
                             expense_values=expense_values, expense_dates=expense_dates, filtered_revenue_values=filtered_revenue_values,
-                            filtered_expense_values=filtered_expense_values)
+                            filtered_expense_values=filtered_expense_values, outstanding_income_invoices=outstanding_income_invoices,
+                            outstanding_expense_invoices=outstanding_expense_invoices, outstandingIncomeTotal=outstandingIncomeTotal,
+                            outstandingExpenseTotal=outstandingExpenseTotal)
 
 
 @app.route('/about')
